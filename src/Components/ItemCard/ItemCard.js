b7ef7ebca6d8fdaf0158a5a591400f61b5236d84
darmun <darmun@protonmail.com>
2019-04-13 21:22:45 +0200

    added card flip

--- src/Components/ItemCard/ItemCard.js
+++ src/Components/ItemCard/ItemCard.js
@@ -4,7 +4,7 @@ import { withStyles } from "@material-ui/core/styles";
 import classnames from "classnames";
 import ExpandMore from "@material-ui/icons/ExpandMore";
 import IconButton from "@material-ui/core/IconButton";
-import testCar from './testImg.jpg';
+import testCar from "./testImg.jpg";
 
 const styles = theme => ({
   expand: {
@@ -24,7 +24,23 @@ const styles = theme => ({
   image: {
     display: "block",
     width: "100%",
-    height: "auto",
+    height: "auto"
+  },
+  overlay: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "#008CBA",
+    overflow: "hidden",
+    width: "100%",
+    height: 0,
+    transition: theme.transitions.create("transform", {
+        duration: theme.transitions.duration.shortest
+      })
+  },
+  overlayVisible:{
+    height: "100%",
   }
 });
 
@@ -38,19 +54,26 @@ class ItemCard extends React.Component {
   render() {
     const { classes } = this.props;
     return (
-      <div className={classnames(classes.container)}>
-      <img src={testCar} alt="Car" className={classnames(classes.image)}/>
-          <div>
-        <IconButton
-          className={classnames(classes.expand, {
-            [classes.expandOpen]: this.state.expanded
-          })}
-          onClick={this.handleExpandClick}
-          aria-expanded={this.state.expanded}
-          aria-label="Show more"
-        >
-          <ExpandMore />
-        </IconButton>
+      <div>
+        <div className={classnames(classes.container)}>
+          <img src={testCar} alt="Car" className={classnames(classes.image)} />
+          <div  className={classnames(classes.overlay, {
+              [classes.overlayVisible]: this.state.expanded
+            })}>
+            <div class="text">Hello shit show World</div>
+          </div>
+        </div>
+        <div>
+          <IconButton
+            className={classnames(classes.expand, {
+              [classes.expandOpen]: this.state.expanded
+            })}
+            onClick={this.handleExpandClick}
+            aria-expanded={this.state.expanded}
+            aria-label="Show more"
+          >
+            <ExpandMore />
+          </IconButton>
         </div>
       </div>
     );
