9154479481311501c5c001082d3a5c0013605d1d
darmun <darmun@protonmail.com>
2019-04-24 16:02:57 +0200

    added basic search form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 import ItemCard from "./Components/ItemCard/ItemCard.js";
+import SearchForm from "./Components/SearchForm";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ItemCard />
+        <SearchForm />
+       
       </div>
     );
   }
--- src/Components/ItemCard/Illustration.js
+++ src/Components/ItemCard/Illustration.js
@@ -2,12 +2,16 @@ import React from "react";
 import CardHeader from "@material-ui/core/CardHeader";
 import testCar from "./testImg.jpg";
 
+const imgStyle = {
+  width: "100%",
+  height: "auto",
+}
 
 export default function Illustration({info}) {
   return (
     <div>
       <CardHeader title={info} />
-      <img src={testCar} alt="Car" className="image" />
+      <img src={testCar} alt="Car" style={imgStyle} />
     </div>
   );
 }
